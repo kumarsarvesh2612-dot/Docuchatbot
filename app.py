323adabfb88c888273e3e364107df53e60218840
kumarsarvesh2612-dot <kumarsarvesh2612@gmail.com>
2026-08-21 01:38:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 load_dotenv()
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
