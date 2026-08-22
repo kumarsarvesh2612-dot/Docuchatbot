488b91b2b6e79505e436f094fb7064b9f9869734
kumarsarvesh2612-dot <kumarsarvesh2612@gmail.com>
2026-08-22 11:47:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -305,4 +305,5 @@ def feedback():
             return render_template("feedback.html",error="Feedback could not be saved.")
     return render_template("feedback.html")
 if __name__=="__main__":
-     app.run(debug=True , use_reloader=False)
+     app.run(host="0.0.0.0:=",port=int(os.environ.get("PORT",5000)))
+            
